bfd61d1bf4bc3366b6817c099b9892fc23d9cf09
md mohsin <siam.mohsin2005@gmail.com>
2025-10-27 16:16:19 +0600

    update config structure, improve dataset loader, and enhance utility functions

--- codeInsight/data/dataset_builder.py
+++ codeInsight/data/dataset_builder.py
@@ -1,23 +1,56 @@
 from codeInsight.logger import logging
 from codeInsight.exception import ExceptionHandle
-from codeInsight.utils.config import load_config
 from datasets import load_dataset
-import os
+import torch
 import sys
 
-def load_dataset():
-    try:
-        logging.info("Downloading dataset from huggingface")
-        config = load_config("config/schema.yaml")
-        
-        data = load_dataset(config['dataset']['name'])
-        train_data = data['train']
-        
-        subset = train_data.shuffle(seed=42).select(range(20000))
-        subset = subset.remove_columns(['input'])
-        
-        return subset
+class DatasetBuilder:
+    def __init__(self, config : dict, tokenizer):
+        self.config = config['dataset']
+        self.tokenizer = tokenizer
+        logging.info("DatasetBuilder initialized")
     
-    except Exception as e:
-        logging.error("Error loading dataset")
-        raise ExceptionHandle(e, sys)
+    def _format_example(self, example : dict) -> dict:
+        try:
+            prompt_template = self.config['prompt_template']
+            text = prompt_template.format(
+                instruction=example['instruction'],
+                output=example['output']
+            )
+            return {"text" : text}
+        
+        except Exception as e:
+            logging.error("Something is wrong in format_example")
+            raise ExceptionHandle(e, sys)
+    
+    def _tokenize_batch(self, batch : dict) -> dict:
+        return self.tokenizer(
+            batch['text'],
+            padding=True,
+            truncation=True,
+            return_tensors=None
+        )
+    
+    def get_tokenized_dataset(self) -> dict:
+        try:
+            dataset_name = self.config['name']
+            raw_data = load_dataset(dataset_name)
+            logging.info(f"Load dataset: {dataset_name}")
+            
+            tokenized_dataset = {}
+            
+            for split in raw_data.keys():
+                formatted_data = raw_data[split].map(
+                    self._format_example,
+                    remove_columns=['instruction', 'output']
+                )
+                if split=="train":
+                    formatted_data = formatted_data.shuffle(seed=self.config['shuffle_seed'])
+                    
+                tokenized_dataset[split] = formatted_data
+            logging.info("All datasets processed and tokenized.")
+            return tokenized_dataset
+        
+        except Exception as e:
+            logging.error(f"Failed during dataset preparation: {e}")
+            raise ExceptionHandle(e, sys)
--- codeInsight/utils/config.py
+++ codeInsight/utils/config.py
@@ -1,15 +1,20 @@
 from codeInsight.exception import ExceptionHandle
 from codeInsight.logger import logging
+from pathlib import Path
 import sys
 import yaml
 
-def load_config(path : str):
+def load_config(config_path : Path = Path("config/model.yaml")) -> dict:
     try:
-        with open(path, "r") as yaml_file:
+        with open(config_path, "r") as yaml_file:
             config = yaml.safe_load(yaml_file)
-            logging.info(f"Config loaded from {path}")
+            logging.info(f"Config loaded from {config_path}")
         return config
     
+    except FileNotFoundError:
+        logging.error(f"Configuration file not found at: {config_path}")
+        raise ExceptionHandle(e, sys)
+    
     except Exception as e:
-        logging.error(f"Error loading config from {path}")
+        logging.error(f"Error loading config from {config_path}")
         raise ExceptionHandle(e, sys)
